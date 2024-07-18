acd72d9e11ad2132d5bd6621a57882464dc50851
Yomin <yomin.lin@itstar.tw>
2024-07-18 14:39:48 +0800

    Initialize the project.

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.find_space
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:find_space/app/app.dart';
+import 'package:find_space/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
